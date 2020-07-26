bda67b43c240ee6c13165f1b8e63c15c457778dd
Today2000 <68829553+Today2000@users.noreply.github.com>
2020-07-27 01:13:08 +0200

    Temp desired not working
    The Subscribe Topics is different than the documentation

--- src/main.cpp
+++ src/main.cpp
@@ -111,7 +111,7 @@ void setup() {
     mqttClient->addPublisher("spa/state/heatreached",  []() -> uint8_t  { return controlPanel->isHeatReached(); });
     mqttClient->addPublisher("spa/state",              []() -> uint16_t { return controlPanel->getRawStatus(); });
 
-    mqttClient->addSubscriber("spa/temp/desired/set",  [](uint16_t v) -> bool { return controlPanel->setDesiredTemperatureCelsius(v); });
+    mqttClient->addSubscriber("spa/state/temp/desired/set",  [](uint16_t v) -> bool { return controlPanel->setDesiredTemperatureCelsius(v); });
     mqttClient->addSubscriber("spa/state/power/set",   [](bool v) -> bool { return controlPanel->setPowerOn(v); });
     mqttClient->addSubscriber("spa/state/filter/set",  [](bool v) -> bool { return controlPanel->setFilterOn(v); });
     mqttClient->addSubscriber("spa/state/heater/set",  [](bool v) -> bool { return controlPanel->setHeaterOn(v); });
